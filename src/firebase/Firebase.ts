b2ad1d4cf4c9073e2903ada772ff78d1b19a719f
T-aka0427 <takahiro.engineer@gmail.com>
2022-03-22 21:24:09 +0900

    202203222000

--- src/firebase/Firebase.ts
+++ src/firebase/Firebase.ts
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCmvniByANWLr18SNpPmYNX3Wm3jDeFG3w",
-  authDomain: "degitallibrary.firebaseapp.com",
-  projectId: "degitallibrary",
-  storageBucket: "degitallibrary.appspot.com",
-  messagingSenderId: "801200907372",
-  appId: "1:801200907372:web:94cc62d466723aedb02449",
-  measurementId: "G-0MNDJY2Y3P",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
